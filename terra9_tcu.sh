39748f6c303c6f855343d456e501851c5ac317cf
David 'Erosion' Oberlin <40402624+DieSkaarj@users.noreply.github.com>
2023-06-04 16:28:22 +0100

    Add files via upload

--- terra9_tcu.sh
+++ terra9_tcu.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 # Quick and dirty shell script to concatenate some textures from Doom2 into Terra9.wad
 # You will need to place both iwads in the input directory.
+mkdir temp
 mkdir output
-deutex -overwrite -dir ./output -doom2 ./input -flats -patches -extract ./input/doom2.wad
-deutex -overwrite -dir ./output -doom ./input -textures -extract ./input/doom.wad
-mkdir ./output/textures
-mkdir ./output/graphics
-cp ./input/data/textures/terra9.txt ./output/textures
-cp ./input/data/graphics/* ./output/graphics
-cp ./input/data/patches/* ./output/patches
-deutex -overwrite -dir ./output -doom ./input -build ./input/data/terra9.txt temptex.wad
-rm -r ./output
-deutex -doom ./input -join terra9.wad temptex.wad
-rm temptex.wad
+deutex -overwrite -dir ./temp -doom2 ./input -flats -patches -extract ./input/doom2.wad
+deutex -overwrite -dir ./temp -doom ./input -textures -extract ./input/doom.wad
+mkdir ./temp/graphics
+cp ./input/data/textures/terra9.txt ./temp/textures
+cp ./input/data/graphics/* ./temp/graphics
+cp ./input/data/patches/* ./temp/patches
+deutex -overwrite -dir ./temp -doom ./input -build ./input/data/terra9info.txt temp/temptex.wad
+deutex -doom ./input -join ./input/data/terra9.wad temp/temptex.wad
+rm -r ./temp
+cp ./input/data/terra9.wad output
+cp ./input/data/terra9.deh output
